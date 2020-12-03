0234bed832d1afad70f281ba80ece6080a23fb4a
Raul Romo <rromomxdev@gmail.com>
2020-12-03 01:30:20 -0600

    feat: add mocha testing

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -6,10 +6,76 @@ const Product = coTest.Product;
 
 describe("Co Test", function() {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  it("Normal Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Medium Coverage", 10, 20) ]);
     const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    expect(products[0].price).equal(19);
+  });
+
+  it("Normal Coverage for 1 day, date passed", function() {
+    const coTest = new CarInsurance([ new Product("Medium Coverage", -1, 20) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(18);
+  });
+
+  it("Full Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Full Coverage", 2, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(1);
+  });
+
+  it("Full Coverage for 1 day, date passed", function() {
+    const coTest = new CarInsurance([ new Product("Full Coverage", -1, 20) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(22);
+  });
+
+  it("Low Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Low Coverage", 5, 7) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(6);
+  });
+
+  it("Mega Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Mega Coverage", 0, 80) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(80);
+  });
+
+  it("Special Full Coverage Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 15, 20) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(21);
+  });
+
+  it("Special Full Coverage Coverage for 1 day, date < 10 days", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 9, 15) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(17);
+  });
+
+  it("Special Full Coverage Coverage for 1 day, date < 5 days", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", 4, 15) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(18);
+  });
+
+  it("Special Full Coverage Coverage for 1 day, date < 0 days", function() {
+    const coTest = new CarInsurance([ new Product("Special Full Coverage", -1, 15) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(0);
+  });
+
+  it("Super Sale Coverage for 1 day", function() {
+    const coTest = new CarInsurance([ new Product("Super Sale", 3, 6) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(4);
+  });
+
+  it("Super Sale Coverage for 1 day, date passed", function() {
+    const coTest = new CarInsurance([ new Product("Super Sale", -1, 16) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).equal(12);
   });
 
 });
--- src/coapp.js
+++ src/coapp.js
@@ -1,15 +1,15 @@
-const { Product, CarInsurance } =require('./coTest.js');
+const { Product, CarInsurance } = require('./coTest.js');
 
 const productsAtDayZero = [
-  new Product('Medium Coverage', 10, 20),
+  new Product('Medium Coverage', -1, 20),
   new Product('Full Coverage', 2, 0),
   new Product('Low Coverage', 5, 7),
   new Product('Mega Coverage', 0, 80),
   new Product('Mega Coverage', -1, 80),
   new Product('Special Full Coverage', 15, 20),
-  new Product('Special Full Coverage', 10, 49),
+  new Product('Special Full Coverage', 10, 10),
   new Product('Special Full Coverage', 5, 49),
-  new Product('Super Sale', 3, 6),
+  new Product('Super Sale', -1, 16),
 ];
 
 const carInsurance = new CarInsurance(productsAtDayZero);
@@ -18,9 +18,6 @@ const productPrinter = function (product) {
 };
 
 for (let i = 1; i <= 30; i += 1) {
-  if(i == 11) {
-    debugger
-  }
   console.log(`Day ${i}`);
   console.log('name, sellIn, price');
   carInsurance.updatePrice().forEach(productPrinter);
